693b458274bdbdad1a9dab6f3a2d9bf81ec4d173
MmSARGIN <melih.sargin.53@gmail.com>
2022-11-14 22:21:21 +0300

    first commit

---
+++ src/components/charts/DonutChart.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+ 
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+const options = ref({});
+const series = ref([44, 55, 41, 17, 15]);
+</script>
